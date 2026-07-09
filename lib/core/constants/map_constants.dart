dcc2aad86dc42cedc08e74f158699aa5c18bb2f2
Shoaib-1965 <shoaibaltaf92pk@gmail.com>
2026-07-09 16:51:42 +0500

    fix: add gradle.properties to gitignore, add example template

--- lib/core/constants/map_constants.dart
+++ lib/core/constants/map_constants.dart
@@ -1,4 +1,4 @@
 class MapConstants {
-  static const String mapboxAccessToken = 'YOUR_MAPBOX_SECRET_TOKEN';
+  static const String mapboxAccessToken = 'YOUR_MAPBOX_PUBLIC_TOKEN_HERE';
   static const String mapboxStyleUri = 'mapbox://styles/shoaib1965/cmqm2b0qd009m01s1h9773jnl';
 }
